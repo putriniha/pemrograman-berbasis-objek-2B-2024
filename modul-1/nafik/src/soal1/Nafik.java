7cf8690b2f7df24dbf58d86cec8b99edd8df6ba0
Nafik-Ardat <nafikadventur@gmail.com>
2024-03-23 01:50:37 +0700

    230-115_nafik ali yahya mungumpulkan tugas soal 1 kak rakha

---
+++ modul-1/nafik/src/soal1/Nafik.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Nafik {
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        Manusia manusia1 = new Manusia("napik", 25, "Lamogan");
+        Manusia manusia2 = new Manusia("kepin", 30, "Bandung");
+        Manusia manusia3 = new Manusia("joni", 22, "Surabaya");
+
+        
+        manusia1.berjalan(); 
+        manusia2.berlari(); 
+        manusia3.jogging();
+        
+    }
+    
+}
