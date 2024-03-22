f84965d6aa0465e12cda92ce349fb2708b55e038
Nafik-Ardat <nafikadventur@gmail.com>
2024-03-23 02:48:10 +0700

    230-115_nafik ali yahya mungumpulkan tugas soal 1 kak rakha

---
+++ modul-1/230441100115-NafikAliYahya/src/pkg230441100115/nafikaliyahya/NafikAliYahya.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg230441100115.nafikaliyahya;
+
+/**
+ *
+ * @author Administrator
+ */
+public class NafikAliYahya {
+
+    public static void main(String[] args) {
+        Manusia manusia1 = new Manusia("nafik", 20, "lamongan");
+        Manusia manusia2 = new Manusia("kepin", 19, "meduro");
+        Manusia manusia3 = new Manusia("joni", 18, "jepang");
+        
+        manusia1.berjalan1();
+        manusia2.berlari();
+        manusia3.berjalan2();
+    }    
+}
