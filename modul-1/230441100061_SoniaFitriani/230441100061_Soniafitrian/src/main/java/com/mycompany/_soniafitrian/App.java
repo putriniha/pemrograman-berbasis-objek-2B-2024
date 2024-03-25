83d3511393dc735e36871b080dd681d63b27e552
Soniafit <sugikyanto586@gmail.com>
2024-03-25 23:00:19 +0700

    mengumpulkan tugas1 mudul 1 asprak kk raka

---
+++ modul-1/230441100061_SoniaFitriani/230441100061_Soniafitrian/src/main/java/com/mycompany/_soniafitrian/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._soniafitrian;
+
+/**
+ *
+ * @author sonia
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
