255bc5a4d112c26c9cef9f68aaaa49d1ea8205ae
Soniafit <sugikyanto586@gmail.com>
2024-03-25 23:00:58 +0700

    mengumpulkan tugas1 modul 1 asprak kk raka

---
+++ modul-1/230441100061_Sonia/src/pkg230441100061_sonia/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg230441100061_sonia;
+
+import static pkg230441100061_sonia.kendaraan.roda;
+
+/**
+ *
+ * @author sonia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        kendaraan motor = new kendaraan();
+        motor.warna(warna:"biru");
+        Integer roda;
+        kendaraan.roda(roda:2);
+        motor.tambah cc ()
+    }
+    
+}
---
+++ modul-1/230441100061_Sonia/src/pkg230441100061_sonia/kendaraan.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg230441100061_sonia;
+
+/**
+ *
+ * @author sonia
+ */
+public class kendaraan {
+
+    /**
+     *
+     * @param warna
+     */
+    public  void kendaraaan (String warna){
+        System.out.println ("Warnanya;" + warna);
+     }
+
+    /**
+     *
+     * @param roda
+     */
+    public  void roda (Integer roda){
+       System.out.println(" Rodanya ada;"+roda);   
+
+    }
+}
+            
