e4f41f1ccc9ce0864a2b507e7e8619087629529b
Nafik-Ardat <nafikadventur@gmail.com>
2024-03-25 10:43:19 +0700

    230-115_nafik ali yahya mungumpulkan tugas soal 1 kak rakha

--- modul-1/230441100115-NafikAliYahya/src/pkg230441100115/nafikaliyahya/NafikAliYahya.java
+++ modul-1/230441100115-NafikAliYahya/src/pkg230441100115/nafikaliyahya/NafikAliYahya.java
@@ -8,6 +8,40 @@ package pkg230441100115.nafikaliyahya;
  *
  * @author Administrator
  */
+class Manusia{
+    
+    String nama;
+    int umur;
+    String alamat;
+    
+    public Manusia(String nama, int umur, String alamat){
+        this.nama = nama;
+        this.umur = umur;
+        this.alamat = alamat;  
+    }
+    
+    public void berjalan1(){
+        System.out.println("nama : " + nama);
+        System.out.println("umur : " + umur);
+        System.out.println("alamat : " + alamat);
+        System.out.println("saat ini sedang berjalan\n");
+    } 
+    
+    public void berlari(){
+        System.out.println("nama : " + nama);
+        System.out.println("umur : " + umur);
+        System.out.println("alamat : " + alamat);
+        System.out.println("saat ini sedang berlari\n");
+    } 
+    
+    public void berjalan2(){
+        System.out.println("nama : " + nama);
+        System.out.println("umur : " + umur);
+        System.out.println("alamat : " + alamat);
+        System.out.println("saat ini sedang berjalan");
+    } 
+}
+
 public class NafikAliYahya {
 
     public static void main(String[] args) {
