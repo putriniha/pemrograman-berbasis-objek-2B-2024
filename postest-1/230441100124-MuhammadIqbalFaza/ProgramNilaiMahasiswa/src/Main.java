f8b9d5f73114150a7aecc58d973ac9e85f77e1db
imuh17 <imuh6500@gmail.com>
2024-03-30 11:11:02 +0700

    Tugas POSTEST1

--- postest-1/230441100124-MuhammadIqbalFaza/ProgramNilaiMahasiswa/src/Main.java
+++ postest-1/230441100124-MuhammadIqbalFaza/ProgramNilaiMahasiswa/src/Main.java
@@ -3,7 +3,5 @@ public class Main {
     public static void main(String[] args) {
         Mahasiswa mahasiswa1 = Mahasiswa.inputDataMahasiswa();
         mahasiswa1.ShowDataMahasiswa();
-//        System.out.printf("Hello and welcome!");
-
     }
 }
