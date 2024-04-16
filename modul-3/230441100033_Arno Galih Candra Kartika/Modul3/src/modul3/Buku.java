5b9f90696fe0a34cc8f17966d543998669888675
arnogalih <arnogalih@gmail.com>
2024-04-16 10:40:44 +0700

    230441100033_ArnoGalihCandraKartika_Modul3

---
+++ modul-3/230441100033_Arno Galih Candra Kartika/Modul3/src/modul3/Buku.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul3;
+
+/**
+ *
+ * @author arno
+ */
+public class Buku {
+    String Judul, Penulis, Publisher, Kategori;
+    int Stok, TahunTerbit;
+
+    public Buku(String Judul, String Penulis, String Publisher, String Kategori, int Stok, int TahunTerbit) {
+        this.Judul = Judul;
+        this.Penulis = Penulis;
+        this.Publisher = Publisher;
+        this.Kategori = Kategori;
+        this.Stok = Stok;
+        this.TahunTerbit = TahunTerbit;
+    }
+}
