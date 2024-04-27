f38fa9a9995041b19a72fca34f359bd6080e32a1
meifriska <meifriska13@.com>
2024-04-27 23:14:02 +0700

    230441100088_MeiFriska_modul3

---
+++ modul-3/230441100088_MeiFriska_modul3/src/main/java/com/mycompany/modul3/tumbuhan_air.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modul3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class tumbuhan_air extends tumbuhan{
+    public void tempat_hidup(){
+        System.out.println("hidup di air");
+    }
+    
+}
