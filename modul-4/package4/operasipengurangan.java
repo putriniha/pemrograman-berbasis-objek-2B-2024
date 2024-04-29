e70842eb8e23cc093201bd3c836163d0a4df253d
Ssuryabaihaqi480 <suryabaihaqiahmad@gmail.com>
2024-04-29 13:20:30 +0700

    SURYA BAIHAQI AHMAD-230441100082_Modul 4_kak rakha

---
+++ modul-4/package4/operasipengurangan.java
@@ -0,0 +1,44 @@
+
+package package4;
+
+
+
+
+class OperasiPengurangan extends OperasiAbstract{
+    @Override
+    protected void set_A(double a) {
+        this.a = a;
+    }
+
+    @Override
+    protected void set_B(double b) {
+        this.b = b;
+    }
+
+    @Override
+    protected void set_C() {
+        this.c = a - b;
+    }
+
+    @Override
+    protected double get_A() {
+        return a;
+    }
+
+    @Override
+    protected double get_B() {
+        return b;
+    }
+
+    @Override
+    protected double get_C() {
+        return c;
+    }
+
+    @Override
+    protected void tampil() {
+        System.out.println("x Hasil Pengurangan ");
+        System.out.println("x " + get_A() + " - " + get_B() + " = " + get_C());
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    }
+}
---
+++ modul-4/package4/operasipenjumlahan.java
@@ -0,0 +1,42 @@
+
+package package4;
+
+class OperasiPenjumlahan extends OperasiAbstract{
+    protected double a, b, c;
+    @Override
+    protected void set_A(double a) {
+        this.a = a;
+    }
+
+    @Override
+    protected void set_B(double b) {
+        this.b = b;
+    }
+
+    @Override
+    protected void set_C() {
+        this.c = a + b;
+    }
+
+    @Override
+    protected double get_A() {
+        return a;
+    }
+
+    @Override
+    protected double get_B() {
+        return b;
+    }
+
+    @Override
+    protected double get_C() {
+        return c;
+    }
+
+    @Override
+    protected void tampil() {
+        System.out.println("x Hasil penjumlahan ");
+        System.out.println("x " + get_A() + " + " + get_B() + " = " + get_C());
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    }
+}
